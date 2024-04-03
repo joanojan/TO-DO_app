d15f05d8a9417461dfdc7168b06f6019c04ee3cd
Joan Vila Valls <joan.o.jan@gmail.com>
2024-04-03 19:35:57 +0200

    security tryial

--- src/api/supabase.js
+++ src/api/supabase.js
@@ -2,7 +2,6 @@
 const PROJECT_URL = 'https://ysvtdjwtvwdmahyaqunc.supabase.co'
 //API key
 //safe if you have enabled Row Level Security (RLS) for your tables and configured policies
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzdnRkand0dndkbWFoeWFxdW5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE4OTcwMjIsImV4cCI6MjAyNzQ3MzAyMn0.VBXwDxHo9Bxr58IDLmiTEPy5VEIxv2oLwYj6sTUkjws'
 
 import { createClient } from '@supabase/supabase-js'
 
