6c244e7356e6a40ac99fc1075d7efc81d842efac
Joan Vila Valls <joan.o.jan@gmail.com>
2024-04-07 11:04:26 +0200

    SignIn.vue style

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -30,25 +30,30 @@ const signUp = () => {
 
 <template>
 	<main>
-		<form @submit.prevent="signIn">
-			<div class="mb-4">
-				<label for="email" class="block text-sm font-medium text-gray-700">Email:</label>
-				<input type="email" id="email" v-model="user" placeholder="Email"
-					class="form-input w-full rounded-md border border-gray-300 py-2 px-3 text-sm shadow-outline focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
-			</div>
-			<div class="mb-4">
-				<label for="password" class="block text-sm font-medium text-gray-700">Password:</label>
-				<input type="password" id="password" v-model="password" placeholder="Password"
-					class="form-input w-full rounded-md border border-gray-300 py-2 px-3 text-sm shadow-outline focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
-			</div>
-			<ButtonComponent type="submit"
-				class="w-full bg-indigo-500 text-white rounded-md py-2 px-4 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-				Sign In</ButtonComponent>
-			<div v-if="userStore.error" class="text-red-500 text-sm mt-2">
-				{{ userStore.error.message }}
-			</div>
-		</form>
+		<div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
 
-		<SignUpPrompt @click="signUp" />
+			<form @submit.prevent="signIn">
+				<div class="mb-4">
+					<label for="email" class="block my-2 text-sm font-medium leading-6 text-gray-900">Email address</label>
+					<input type="email" id="email" v-model="user" placeholder="Email"
+						class="form-input w-full rounded-md border border-gray-300 py-2 px-3 text-sm">
+				</div>
+				<div class="mb-4">
+					<label for="password" class="block text-sm font-medium leading-6 my-2 text-gray-900">Password</label>
+					<input type="password" id="password" v-model="password" placeholder="Password"
+						class="form-input w-full rounded-md border border-gray-300 py-2 px-3 text-sm shadow-outline focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
+				</div>
+				<ButtonComponent type="submit"
+					class="w-full bg-indigo-600 text-white rounded-md py-2 px-4 hover:bg-indigo-500 focus:outline-none">
+					Sign In</ButtonComponent>
+				<div v-if="userStore.error" class="text-red-500 text-sm mt-2">
+					{{ userStore.error.message }}
+				</div>
+			</form>
+
+			<SignUpPrompt @click="signUp" />
+
+		</div>
+
 	</main>
 </template>
