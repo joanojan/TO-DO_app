9bf54cf00a3349918dc015853b2c92cee4466de9
Joan Vila Valls <joan.o.jan@gmail.com>
2024-04-20 14:42:48 +0200

    about style

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,38 +1,72 @@
 <template>
 	<main>
-		<h1 class="p-6 bg-gray-500 text-3xl text-white text-center">
+		<h1 class="p-4 bg-gray-500 text-3xl text-white text-center">
 			IRON HACK FINAL PROJECT
 		</h1>
 		<section class="flex my-2 text-xl">
 			<div class="mx-auto">
 				<h1 class="text-3xl bg-pink-400 text-center p-2 text-slate-200">Crafted with love - Joan Vila Valls -
 				</h1>
-				<div class="flex justify-around my-4">
-					<a class="text-decoration-line text-slate-500 hover:text-pink-400"
-						href="https://main--todo-app-by-jvv.netlify.app/">
+				<div class="flex justify-evenly my-6">
+					<a class="text-slate-500 hover:text-pink-400" href="https://main--todo-app-by-jvv.netlify.app/">
 						Live website
 					</a>
-					<a class="text-decoration-line text-slate-500 hover:text-pink-400"
-						href="https://github.com/joanojan/TO-DO_app">
+					<a class="text-slate-500 hover:text-pink-400" href="https://github.com/joanojan/TO-DO_app">
 						GitHub repo
 					</a>
 				</div>
-				<section class="flex-col">
-					<p class="text-center font-bold">Single Page Application (SPA) of the classic TO-DO app:</p>
-					<ul class="m-4 list-disc ml-20">
-						<li>users can sing-up, sign-in, sign-out</li>
-						<li>CRUD -> create, read, update and delete tasks. </li>
+				<section class="flex-col my-4">
+					<p class="text-center font-bold my-4">Single Page Application - TO-DO app</p>
+					<ul class="text-center">
+						<li>-- users can sing-up, sign-in, sign-out --</li>
+						<li>-- create, read, update and delete tasks -- </li>
 					</ul>
-					<p>Backend support: supabase (open source alternative to Firebase)</p>
-					<div class="flex-col mx-10">
-						<h3 class="my-4 font-bold text-2xl">-- Frontend stack --</h3>
-						<ul class="list-disc ml-10">
-							<li>Framework: Vue3</li>
-							<li>Build tool: Vite</li>
-							<li>Routing: Vue Router</li>
-							<li>State managment: Pinia</li>
-							<li>CSS framework: Tailwind</li>
-						</ul>
+					<p class="my-4 font-bold">Backend support: supabase (open source alternative to Firebase)</p>
+					<div class="flex justify-center">
+						<div class="flex-col">
+							<table class="table-auto">
+								<h3 class="my-4 font-bold text-2xl text-center">-- Frontend stack --</h3>
+								<tbody>
+									<tr>
+										<td class="px-4 py-2">Framework:</td> <!-- Added padding classes -->
+										<td class="px-4 py-2">
+											<a class="text-slate-500 hover:text-pink-400"
+												href="https://vuejs.org/">Vue3</a>
+										</td>
+									</tr>
+									<tr>
+										<td class="px-4 py-2">Build tool:</td>
+
+										<td class="px-4 py-2">
+											<a class="text-slate-500 hover:text-pink-400"
+												href="https://vitejs.dev/">Vite</a>
+										</td>
+									</tr>
+									<tr>
+										<td class="px-4 py-2">Routing:</td>
+
+										<td class="px-4 py-2">
+											<a class="text-slate-500 hover:text-pink-400"
+												href="https://router.vuejs.org/">Vue Router</a>
+										</td>
+									</tr>
+									<tr>
+										<td class="px-4 py-2">State management:</td>
+										<td class="px-4 py-2">
+											<a class="text-slate-500 hover:text-pink-400"
+												href="https://pinia.vuejs.org/">Pinia</a>
+										</td>
+									</tr>
+									<tr>
+										<td class="px-4 py-2">CSS framework:</td>
+										<td class="px-4 py-2">
+											<a class="text-slate-500 hover:text-pink-400"
+												href="https://tailwindcss.com/">Tailwind</a>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
 					</div>
 				</section>
 			</div>
