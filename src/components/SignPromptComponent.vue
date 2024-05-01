c2817dad831950bbce2efa190c647bbdf219c01c
Joan Vila Valls <joan.o.jan@gmail.com>
2024-05-01 16:33:50 +0200

    responsive design

--- src/components/SignPromptComponent.vue
+++ src/components/SignPromptComponent.vue
@@ -8,7 +8,7 @@ const clickAction = props.signIn? 'signin' : 'signup'
 </script>
 
 <template>
-    <div class="sm:mt-10 flex justify-evenly items-center justify-center h-8 bg-white dark:bg-slate-900 p-2 rounded">
+    <div class="sm:mt-10 my-2 flex justify-evenly items-center justify-center h-8 bg-white dark:bg-slate-900 p-2 rounded">
         <p v-if="!signIn" class="md:text-lg text-sm dark:text-white font-bold">Don't have an account?</p>
         <p v-if="signIn" class="md:text-lg dark:text-white font-bold">Already have an account?</p>
         <button 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,9 @@ import ButtonComponent from '@/components/ButtonComponent.vue';
 </script>
 
 <template>
-    <section class="max-w-screen-lg mx-auto">
-        <h1 class="flex justify-center dark:text-white font-bold text-2xl my-10">Welcome to your Tasks App</h1>
-        <router-link to="/tasks" class="flex justify-center">
+    <section class="mb:w-60 bg-gray-300 shadow-2xl rounded-xl max-w-sm  mx-auto">
+        <h1 class="dark:text-black text-center p-2 font-bold text-2xl my-10">Welcome to your Tasks App</h1>
+        <router-link to="/tasks" class="flex pb-2 justify-center">
             <button-component class="bg-black">Go to your Tasks</button-component>
         </router-link>
     </section>
